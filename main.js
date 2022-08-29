0c97e5b319d31ac2e30b0d2be9f396bb611df593
Omri Steiner <omris@multinarity.com>
2022-08-29 11:56:59 +0300

    implement action itself

---
+++ main.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core')
+
+async function main() {
+    try {
+        const artifacts = JSON.parse(core.getInput("artifacts", { required: true }))
+
+        if (artifacts.length != 1) {
+            throw new Error("Artifact count must be 1, got " + artifacts.length)
+        }
+
+        const commit_hash = artifacts[0].workflow_run.head_sha
+        core.info("Found commit hash: " + commit_hash)
+        core.setOutput("commit_hash", commit_hash)
+    } catch (error) {
+        core.error("get-artifact-commit action failed: " + error.message)
+        core.setFailed(error.message)
+    }
+}
+
+main()
