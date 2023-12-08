fdabcf10ec43f16442f8ff9ca652e74a4c414f00
orine <orine@multinarity.com>
2023-12-08 14:26:54 +0200

    add debug info

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ async function main() {
             throw new Error("Artifact count must be 1, got " + artifacts.length)
         }
 
+        core.info(JSON.stringify(artifacts));
         const commit_hash = artifacts[0].workflow_run.head_sha
-        core.info("Found commit hash: " + commit_hash)
+        core.info("Found commit hash: " + commit_hash);
         core.setOutput("commit_hash", commit_hash)
     } catch (error) {
         core.error("get-artifact-commit action failed: " + error.message)
