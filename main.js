6ad1fd46748fbd1ccb4e34a87ea619ea422f571a
orine <orine@multinarity.com>
2023-12-08 14:29:41 +0200

    revert

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ async function main() {
             throw new Error("Artifact count must be 1, got " + artifacts.length)
         }
 
-        core.info(JSON.stringify(artifacts));
         const commit_hash = artifacts[0].workflow_run.head_sha
         core.info("Found commit hash: " + commit_hash);
         core.setOutput("commit_hash", commit_hash)
